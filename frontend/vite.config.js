b069c009e121a07f3636a00cf7ac6a73339632d9
ninaplanetdetroit <66696762+ninaplanetdetroit@users.noreply.github.com>
2026-01-01 18:17:40 -0500

    Fix base path for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ask-planet-detroit/',  // For GitHub Pages
+  base: '/',
   server: {
     proxy: {
       '/api': {
